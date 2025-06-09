95dd837d83f7cf2309741273f5ea783692fc0b7f
yashodhakumari <yashodha.kumari@spoors.in>
2025-06-09 11:07:14 +0530

    updated app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def index():
     return render_template("index.html", answer=answer, full_info=full_info)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
