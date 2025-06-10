2233ed6378fc84d7be6abef7ca94d121fc00ec26
yashodhakumari <yashodha.kumari@spoors.in>
2025-06-10 18:46:38 +0530

    updated  html

--- app.py
+++ app.py
@@ -52,5 +52,8 @@ def index():
 
     return render_template('index.html', response=response)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
